3ee1f35739041c2d13bc64bf9d2b4ad3809805a5
hihihixxx89@gmail.com <hihihixxx89@gmail.com>
2021-07-14 17:59:30 +0900

    Initial commit

---
+++ address_api/settings/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# PROJECT_DIR = conf
+# BASE_DIR = root
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
