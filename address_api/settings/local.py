8a982077f68d5e29748a474d4d03a7514fe28980
hihihixxx89@gmail.com <hihihixxx89@gmail.com>
2021-07-15 22:46:05 +0900

    - setup requirements.txt

--- address_api/settings/local.py
+++ address_api/settings/local.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +29,7 @@ class Secret:
     DEBUG = Bool
     ALLOWED_HOSTS = []
     SECRET_KEY = ''
+    
 """
 DEBUG = Secret.DEBUG
 SECRET_KEY = Secret.SECRET_KEY
@@ -123,9 +124,14 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+STATICFILES_DIRS = [
+]
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
